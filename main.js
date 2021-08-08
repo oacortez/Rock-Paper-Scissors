c687c0122ef1a4a0681867eacbff87105fb6a182
Oscar Cortez <oacortez98@gmail.com>
2021-08-08 14:29:56 -0600

    Add beginning querySelector

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+///////  ⬇️ Query Selectors
+var classicBtn = document.querySelector('#classicBtn');
+var difficultBtn = document.querySelector('#difficultBtn');
+var viewYourClassic = document.querySelector('#classicFighters')
+var rockImgBtn = document.querySelector('#rock');
+var paperImg = document.querySelector('#paper');
+var scissorsImg = document.querySelector('#scissors');
+var lizardImg = document.querySelector('#lizard');
+var alienImg = document.querySelector('#alien');
+var showDifficultFighters = document.querySelector('#showDifficultFighters');
+var header = document.querySelector('h2');
