afee0290ed0203d7030626fa3c26882ce50bb94d
Oscar Cortez <oacortez98@gmail.com>
2021-08-10 00:23:46 -0600

    Add new methods to class

--- player.js
+++ player.js
@@ -3,15 +3,30 @@ class Player {
     this.name = name;
     this.emoji = emoji;
     this.wins = 0;
-    this.choice = undefined;
+    this.choice = '';
   }
+
   saveWinsToStorage() {
-
+  localStorage.setItem(this.name, JSON.stringify(this.wins));
   }
+
   retrieveWinsFromStorage() {
+   var wins = localStorage.getItem(this.name);
+   var parsedWins = JSON.parse(wins);
+   if (!wins) {
+    return this.wins = 0;
+   }
+   this.wins = parsedWins;
+   return this.wins;
+ }
 
-  }
   takeTurn() {
-
+    if (this.name === 'Human') {
+      return this.fighter;
+    } else if (this.name === 'Computer') {
+      var fighters = game.fighters;
+      this.fighter = fighters[Math.floor(Math.random()*fighters.length)];
+      return this.fighter;
+    }
   }
 }
